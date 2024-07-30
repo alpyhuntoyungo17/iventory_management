6d11707b05fb64de1adeed3f84d69fe847cea41a
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:39:32 +0800

    Update Navbar.js

--- frontend/src/componenst/Navbar.js
+++ frontend/src/componenst/Navbar.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/orders">Orders</Link></li>
+        <li><Link to="/users">Users</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
 
