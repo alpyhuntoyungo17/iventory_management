5374bd6ac9830a59b889f929449fee2bdae1e01a
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:42:05 +0800

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Web App</h1>
+      <p>This is the home page of the application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
 
