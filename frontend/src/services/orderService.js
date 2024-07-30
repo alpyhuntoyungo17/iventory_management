2daedfe937e5c75a81865e228d053954e8aa1fb7
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:46:54 +0800

    Update orderService.js

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -1,1 +1,55 @@
 
+const Order = require('../models/Order');
+
+const getAllOrders = async () => {
+  try {
+    return await Order.find();
+  } catch (error) {
+    throw new Error('Error fetching orders: ' + error.message);
+  }
+};
+
+const getOrderById = async (id) => {
+  try {
+    const order = await Order.findById(id);
+    if (!order) {
+      throw new Error('Order not found');
+    }
+    return order;
+  } catch (error) {
+    throw new Error('Error fetching order: ' + error.message);
+  }
+};
+
+const createOrder = async (orderData) => {
+  try {
+    const newOrder = new Order(orderData);
+    return await newOrder.save();
+  } catch (error) {
+    throw new Error('Error creating order: ' + error.message);
+  }
+};
+
+const updateOrder = async (id, orderData) => {
+  try {
+    return await Order.findByIdAndUpdate(id, orderData, { new: true });
+  } catch (error) {
+    throw new Error('Error updating order: ' + error.message);
+  }
+};
+
+const deleteOrder = async (id) => {
+  try {
+    return await Order.findByIdAndDelete(id);
+  } catch (error) {
+    throw new Error('Error deleting order: ' + error.message);
+  }
+};
+
+module.exports = {
+  getAllOrders,
+  getOrderById,
+  createOrder,
+  updateOrder,
+  deleteOrder
+};
