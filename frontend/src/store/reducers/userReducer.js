7ab2df1aff530d9aff74705ed5ae808bf7a93301
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:52:20 +0800

    Update userReducer.js

--- frontend/src/store/reducers/userReducer.js
+++ frontend/src/store/reducers/userReducer.js
@@ -1,1 +1,24 @@
 
+import { createReducer } from '@reduxjs/toolkit';
+import { setUsers, addUser, removeUser, updateUser } from '../actions/userAction';
+
+const initialState = [];
+
+const userReducer = createReducer(initialState, {
+  [setUsers]: (state, action) => action.payload,
+  [addUser]: (state, action) => {
+    state.push(action.payload);
+  },
+  [removeUser]: (state, action) => {
+    return state.filter(user => user.id !== action.payload);
+  },
+  [updateUser]: (state, action) => {
+    const { id, data } = action.payload;
+    const index = state.findIndex(user => user.id === id);
+    if (index !== -1) {
+      state[index] = { ...state[index], ...data };
+    }
+  },
+});
+
+export default userReducer;
