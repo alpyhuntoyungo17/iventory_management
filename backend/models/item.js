e2af1768a1526aaf67a3501ee2886bd09e9166a4
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:23:05 +0800

    Update item.js

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
 
