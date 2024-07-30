17ad0561f6c0e27844dee2b55e5237d0a715ff8e
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:51:46 +0800

    Update itemReducer.js

--- frontend/src/store/reducers/itemReducer.js
+++ frontend/src/store/reducers/itemReducer.js
@@ -1,1 +1,24 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { setItems, addItem, removeItem, updateItem } from '../actions/itemAction';
+
+const initialState = [];
+
+const itemReducer = createReducer(initialState, {
+  [setItems]: (state, action) => action.payload,
+  [addItem]: (state, action) => {
+    state.push(action.payload);
+  },
+  [removeItem]: (state, action) => {
+    return state.filter(item => item.id !== action.payload);
+  },
+  [updateItem]: (state, action) => {
+    const { id, data } = action.payload;
+    const index = state.findIndex(item => item.id === id);
+    if (index !== -1) {
+      state[index] = { ...state[index], ...data };
+    }
+  },
+});
+
+export default itemReducer;
 
