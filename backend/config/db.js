1ce46e59ea18b86431bed74a083ae3c4bc213ccd
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:12:01 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,24 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'your-username',
+  password: 'your-password',
+  database: 'exampleDB'
+});
+
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
+
+module.exports = {
+  query
+};
 
