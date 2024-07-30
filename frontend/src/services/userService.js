3e5b56a85cdec0ec5767f730ad4cd8ff993e9ec4
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:46:24 +0800

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,1 +1,55 @@
 
+const User = require('../models/User');
+
+const getAllUsers = async () => {
+  try {
+    return await User.find();
+  } catch (error) {
+    throw new Error('Error fetching users: ' + error.message);
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  } catch (error) {
+    throw new Error('Error fetching user: ' + error.message);
+  }
+};
+
+const createUser = async (userData) => {
+  try {
+    const newUser = new User(userData);
+    return await newUser.save();
+  } catch (error) {
+    throw new Error('Error creating user: ' + error.message);
+  }
+};
+
+const updateUser = async (id, userData) => {
+  try {
+    return await User.findByIdAndUpdate(id, userData, { new: true });
+  } catch (error) {
+    throw new Error('Error updating user: ' + error.message);
+  }
+};
+
+const deleteUser = async (id) => {
+  try {
+    return await User.findByIdAndDelete(id);
+  } catch (error) {
+    throw new Error('Error deleting user: ' + error.message);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};
