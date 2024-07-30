82bfd30fc6db2a8acc9b42edd3280ee72044b232
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:23:44 +0800

    Update order.js

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    item: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    }
+  }],
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
 
