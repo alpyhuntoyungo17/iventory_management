aa5cefee923710eec7a12d064418088f0bb56a2e
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 18:31:53 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+// Import the Express library
+const express = require('express');
+
+// Create an instance of an Express application
+const app = express();
+
+// Define the port the server will listen on
+const port = 3000;
+
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server and listen on the defined port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
