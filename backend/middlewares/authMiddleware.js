5f8d4b76d38c73ca3297aaf7208bf971c19d5647
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:21:13 +0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../keys');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, keys.jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
+
+module.exports = authMiddleware;
 
