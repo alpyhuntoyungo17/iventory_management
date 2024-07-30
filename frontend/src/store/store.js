d97ed689f302be1941c8ba33d8653cdeef5c1e78
Duass <Firdausalistiqomahdomili34@gmail.com>
2024-07-30 19:47:45 +0800

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,1 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
+import itemReducer from './slices/itemSlice';
+import orderReducer from './slices/orderSlice';
+
+// Create the Redux store
+const store = configureStore({
+  reducer: {
+    users: userReducer,
+    items: itemReducer,
+    orders: orderReducer,
+  },
+});
+
+export default store;
 
